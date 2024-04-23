c8ff72508c583407249a81e0b5197d239aee6bcb
Mateusz <105458047+MatZablocki@users.noreply.github.com>
2024-04-23 10:25:10 +0100

    debug draws completed

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -2,7 +2,7 @@
 extends Node3D
 
 @export var length:int = 20
-@export var frequency:float = 0.0
+@export var frequency:float = 1.0
 @export var start_angle:float = 1.0
 @export var base_size:float = 0.5
 @export var multiplier:float = 5.0
@@ -16,11 +16,12 @@ func _process(delta):
 	#if paused:return
 	var position_offset = Vector3.ZERO
 	var current_size = base_size
+	DebugDraw3D.draw_sphere(Vector3.ZERO, base_size*2)
 	for x in range(length):
 		DebugDraw3D.draw_box(Vector3(-current_size/2 + position_offset.x,-current_size/2+position_offset.y,-current_size/2+position_offset.z), Quaternion(), Vector3(current_size,current_size,current_size))
 		position_offset.x+=current_size
 		#position_offset.y = sin(x)
-		current_size += sin(x*frequency) * 0.1
+		current_size += sin(x*frequency+start_angle) * 0.3
 
 	
 	pass		
