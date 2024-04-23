099d1d30da6af2d79e3cdb051e4cd13a8153511f
Mateusz <105458047+MatZablocki@users.noreply.github.com>
2024-04-23 10:55:31 +0100

    trying to get the wanderer scenes to work but nada

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -6,6 +6,10 @@ extends Node3D
 @export var start_angle:float = 1.0
 @export var base_size:float = 0.5
 @export var multiplier:float = 5.0
+@export var head = load("res://head.tscn")
+
+@export var boid : Boid
+var body = load("res://body.tscn")
 
 var paused:bool = true
 
@@ -14,14 +18,9 @@ func _input(event):
 
 func _process(delta):
 	#if paused:return
-	var position_offset = Vector3.ZERO
-	var current_size = base_size
-	DebugDraw3D.draw_sphere(Vector3.ZERO, base_size*2)
-	for x in range(length):
-		DebugDraw3D.draw_box(Vector3(-current_size/2 + position_offset.x,-current_size/2+position_offset.y,-current_size/2+position_offset.z), Quaternion(), Vector3(current_size,current_size,current_size))
-		position_offset.x+=current_size
-		#position_offset.y = sin(x)
-		current_size += sin(x*frequency+start_angle) * 0.1 * multiplier
+	
+	
+	
 
 	
 	pass		
@@ -31,6 +30,20 @@ func _process(delta):
 func _ready():
 	if not Engine.is_editor_hint():		
 		pass
+	var head_inst = head.instantiate()
+	boid.add_child(head_inst)
+	var position_offset = Vector3.ZERO
+	var current_size = base_size
+	#DebugDraw3D.draw_sphere(Vector3.ZERO, base_size*2)
+	for x in range(length):
+		var box = body.instantiate()
+		box.size = Vector3(current_size,current_size,current_size)
+		box.position = Vector3(position_offset.x,0,0)
+		boid.add_child(box)
+		#DebugDraw3D.draw_box(Vector3(-current_size/2 + position_offset.x,-current_size/2+position_offset.y,-current_size/2+position_offset.z), Quaternion(), Vector3(current_size,current_size,current_size))
+		position_offset.x+=current_size
+		#position_offset.y = sin(x)
+		current_size += sin(x*frequency+start_angle) * 0.1 * multiplier
 	
 
 
